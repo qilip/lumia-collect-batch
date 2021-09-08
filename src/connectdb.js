ed3506392fa6a5d7fbc859f10a91a2eccd370083
qilip <qilip@qilip.io>
2021-09-08 07:07:56 +0000

    Upgrade Mongoose 5 -> 6, update axios

--- src/connectdb.js
+++ src/connectdb.js
@@ -4,10 +4,11 @@ export default async () => {
   async function connect(){
     try{
       await mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
+        // Mongoose 6 no longer necessary
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false,
         serverSelectionTimeoutMS: 5000
       })
       .then(console.log('MongoDB Connected'));
